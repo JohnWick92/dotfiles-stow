ae7c5716d50782ec45bec0666c97c362a584e62f
Josué De Oliveira Ferreira <grumec92@gmail.com>
2026-07-31 15:27:15 -0300

    Adding latex support

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -2,3 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.clipboard = "unnamedplus"
+vim.g.vimtex_compiler_latexmk = {
+  build_dir = "build",
+  out_dir = "build",
+}
