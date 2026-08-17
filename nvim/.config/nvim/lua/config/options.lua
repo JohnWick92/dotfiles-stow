a5068a251964f77080c6763fbc6182de580d6678
Josué De Oliveira Ferreira <grumec92@gmail.com>
2026-08-16 22:47:18 -0300

    Updating things cause of ghostty

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -3,16 +3,3 @@
 -- Add any additional options here
 -- ~/.config/nvim/lua/config/options.lua
 vim.opt.clipboard = "unnamedplus" -- yank vai direto pro clipboard
-
--- Configura o OSC52 como provedor de clipboard
-vim.g.clipboard = {
-  name = "OSC 52",
-  copy = {
-    ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
-    ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
-  },
-  paste = {
-    ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
-    ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
-  },
-}
